52b6e90eef33ed7fd57b1403b9d880dbe4aac667
kueter <roodolphe.agbassah@getlinkgroup.com>
2024-05-30 15:43:44 +0200

    Repository:  CRUD added

--- webapp/src/Repositories/ProductRepository.cs
+++ webapp/src/Repositories/ProductRepository.cs
@@ -6,17 +6,34 @@ namespace webapp.src.Repositories;
 
 public interface IProductRepository
 {
+    Task<IEnumerable<Product>> GetProductsAsync();
     Task<Product> GetProductByIdAsync(string id);
+    Task<Product> CreateProduct(Product product);
+    Task UpdateProduct(Product product);
+    Task DeleteProduct(string id);
 }
 
 public class ProductRepository(EcommerceContext context) : IProductRepository
 {
     private readonly EcommerceContext _context = context;
 
+    public async Task<IEnumerable<Product>> GetProductsAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
     public async Task<Product> GetProductByIdAsync(string id)
     {
         return await _context.Products.FindAsync(id);
     }
+
+    public async Task<Product> CreateProduct(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+    
 
     public async Task<Product> UpdateProductAsync(string productId, Product productToUpdate)
     {
@@ -43,5 +60,14 @@ public class ProductRepository(EcommerceContext context) : IProductRepository
         return product;
     }
 
-    
+    public async Task DeleteProduct(string id)
+    {
+        var product = await GetProductByIdAsync(id);
+        if (product != null)
+        {
+            _context.Products.Remove(product);
+           return await _context.SaveChangesAsync();
+        }
+        return null;
+    }
 }

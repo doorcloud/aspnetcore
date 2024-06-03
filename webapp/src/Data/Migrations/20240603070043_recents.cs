fbb27cb0a4ddf69a82c215870b2bf61a83475c0c
kueter <agbassahr@gmail.com>
2024-06-03 09:21:45 +0200

    Product Migration added

---
+++ webapp/src/Data/Migrations/20240603070043_recents.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class recents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Label = table.Column<string>(type: "longtext", nullable: false),
+                    Type = table.Column<string>(type: "longtext", nullable: false),
+                    Stock = table.Column<string>(type: "longtext", nullable: false),
+                    Price = table.Column<string>(type: "longtext", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "longblob", rowVersion: true, nullable: true)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.ComputedColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                })
+                .Annotation("Relational:Collation", "utf8_general_ci");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Name = table.Column<string>(type: "longtext", nullable: false),
+                    Email = table.Column<string>(type: "longtext", nullable: false),
+                    Bithdate = table.Column<string>(type: "longtext", nullable: false),
+                    Phone = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                })
+                .Annotation("Relational:Collation", "utf8_general_ci");
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Ref = table.Column<string>(type: "longtext", nullable: false),
+                    user_id = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Amont = table.Column<string>(type: "longtext", nullable: false),
+                    DeliveryAdresse = table.Column<string>(type: "longtext", nullable: false),
+                    Status = table.Column<string>(type: "longtext", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "longblob", rowVersion: true, nullable: true)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.ComputedColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Orders_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("Relational:Collation", "utf8_general_ci");
+
+            migrationBuilder.CreateTable(
+                name: "OrderRow",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "varchar(255)", nullable: false),
+                    product_id = table.Column<string>(type: "varchar(255)", nullable: true),
+                    RefArticle = table.Column<string>(type: "longtext", nullable: false),
+                    Qte = table.Column<string>(type: "longtext", nullable: false),
+                    OrderID = table.Column<string>(type: "varchar(255)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderRow", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_OrderRow_Orders_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "Orders",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_OrderRow_Products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "Products",
+                        principalColumn: "ID");
+                })
+                .Annotation("Relational:Collation", "utf8_general_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderRow_OrderID",
+                table: "OrderRow",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderRow_product_id",
+                table: "OrderRow",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_user_id",
+                table: "Orders",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderRow");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
